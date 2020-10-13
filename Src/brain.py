db26cc293698f0d3c38dd42139db3d090fea1628
Samuel Laporte <laporte.samuel2000@gmail.com>
2020-10-13 17:41:37 -0400

    Implementation check neck

--- Src/brain.py
+++ Src/brain.py
@@ -4,37 +4,18 @@ from Src.Models.direction import Direction
 from Src.Models.turnInformation import TurnInformation
 from Src.Models.playerPosition import PlayerPosition
 from Src.Models.vector2 import Vector2
-#import numpy as np
+import numpy as np
 
 
 class Brain(metaclass=Singleton):
     # Random variable globale
     #MEMO playerID : turn_info.SelfId, map : turn_info.Map
     step_compter = 0
-    directions_possibles = [Direction._UP, Direction._DOWN, Direction._RIGHT, Direction._LEFT, Direction._NONE]
+    DIRECTIONS_POSSIBLES = [Direction._UP, Direction._DOWN, Direction._RIGHT, Direction._LEFT, Direction._NONE]
+    directions_possibles = DIRECTIONS_POSSIBLES
 
     #remove direction "removeDirection" from array
     #Brain.directions_possibles.pop(directions_possibles.index(removeDirection))
-
-
-
-    #turn_info.Map
-    # directions_possibles = [Direction._UP, Direction._DOWN, Direction._RIGHT, Direction._LEFT, Direction._NONE]
-    # [0,1,2,3]
-    # def verifier_fonction_possible():
-    #     risk = []
-    #     for dir in directions_possibles:
-    #         risk.append(calculer_risk(dir))
-    #     return directions_possibles[argsortmin(risk)]
-    
-    # def calculer_mouvement(directions_possibles):
-
-    # def wall_security(dir):
-    #     if ()
-
-
-
-    #     return action
 
     #MUR
     
@@ -44,46 +25,75 @@ class Brain(metaclass=Singleton):
     #PlayerPosition.Head = vector2()
     #PlayerPosition.Body = []
     #PlayerPosition.Neck = []
-
-    
-
-    
-    def wallcheck(pos, possible_action, turn_map):
+    def wallcheck(player_position_head : Vector2, possible_actions, turn_info):
         new_possible_actions = []
-        
-        for action in possible_action:
+        map2d = np.array(turn_info.Map).reshape((turn_info.MapWidth, turn_info.MapWidth))
+        print(map2d)
+        map2d = map2d.tolist()
+        for action in possible_actions:
             # Check if wall up.
             if action == Direction._UP:
-                if turn_map[player_position.Y + 1][player_position.X] != 'W':
+                if map2d[player_position_head.Y + 1][player_position_head.X] != 'W':
                     new_possible_actions.append(action)
+                    print('UP')
             
             # Check if wall down.
             elif action == Direction._DOWN:
-                if turn_map[player_position.Y - 1][player_position.X] != 'W':
+                if map2d[player_position_head.Y - 1][player_position_head.X] != 'W':
                     new_possible_actions.append(action)
-            
+                    print('DOWN')
             # Check if wall left.
             elif action == Direction._LEFT:
-                if turn_map[player_position.Y - 1][player_position.X - 1] != 'W':
+                if map2d[player_position_head.Y][player_position_head.X - 1] != 'W':
                     new_possible_actions.append(action)
-            
+                    print('LEFT')
             # Check if wall right.
             elif action == Direction._RIGHT:
-                if turn_map[player_position.Y][player_position.X + 1] != 'W':
+                if map2d[player_position_head.Y][player_position_head.X + 1] != 'W':
                     new_possible_actions.append(action)
-            
+                    print('RIGHT')
+        print(len(new_possible_actions))  
         return new_possible_actions
     
-    
+    def neckcheck(player_position_head : Vector2, possible_actions, turn_info):
+        #str(turn_info.SelfId)
+        new_possible_actions = []
+        map2d = np.array(turn_info.Map).reshape((turn_info.MapWidth, turn_info.MapWidth))
+        map2d = map2d.tolist()
+        for action in possible_actions:
+            # Check if wall up.
+            if action == Direction._UP:
+                if map2d[player_position_head.Y + 1][player_position_head.X] != 'p3':
+                    new_possible_actions.append(action)
+                    print('UP')
+            
+            # Check if wall down.
+            elif action == Direction._DOWN:
+                if map2d[player_position_head.Y - 1][player_position_head.X] != 'p3':
+                    new_possible_actions.append(action)
+                    print('DOWN')
+            # Check if wall left.
+            elif action == Direction._LEFT:
+                if map2d[player_position_head.Y][player_position_head.X - 1] != 'p3':
+                    new_possible_actions.append(action)
+                    print('LEFT')
+            # Check if wall right.
+            elif action == Direction._RIGHT:
+                if map2d[player_position_head.Y][player_position_head.X + 1] != 'p3':
+                    new_possible_actions.append(action)
+                    print('RIGHT')
+        print(len(new_possible_actions))  
+        return new_possible_actions
+
     # def calculate_risk(dir):
     
     
-    def get_players_position(id, map, map_width):
+    def get_players_position(turn_info):
         players_position = {}
         c_counter = 0
-        r_counter = map_width - 1
+        r_counter = turn_info.MapWidth - 1
         
-        for pos in map:
+        for pos in turn_info.Map:
             if "P" in pos or "p" in pos:
                 splitPos = pos.split("-")
                 splitPos = filter(lambda s: ("P" in s or "p" in s), splitPos)
@@ -100,26 +110,32 @@ class Brain(metaclass=Singleton):
                         players_position[playerId].Body.append(Vector2(X=c_counter, Y=r_counter))
 
             c_counter += 1
-            if c_counter >= map_width:
+            if c_counter >= turn_info.MapWidth:
                 c_counter = 0
                 r_counter -= 1
         return players_position
 
     
     def on_next_move(turn_info: TurnInformation):
+        Brain.directions_possibles = Brain.DIRECTIONS_POSSIBLES
         '''
         YOUR CODE GOES IN THIS FUNCTION. This is where your AI takes a decision on his next move.
         @param turn_info: Information from the current turn
         @return: The direction your AI chose as his next move.
         '''
-        players = Brain.get_players_position(turn_info.SelfId, turn_info.Map, turn_info.MapWidth) 
-        print("La position de la tete est: ", players[str(turn_info.SelfId)].Head.X, players[str(turn_info.SelfId)].Head.Y)
+        players = Brain.get_players_position(turn_info) 
+        #print("La position de la tete est: ", players[str(turn_info.SelfId)].Head.X, players[str(turn_info.SelfId)].Head.Y)
         
-        print("the game server wants to know your next move and you have the following informations : the id is {0} and the current map is {1} ".format(turn_info.SelfId, turn_info.Map))
+        #print("the game server wants to know your next move and you have the following informations : the id is {0} and the current map is {1} ".format(turn_info.SelfId, turn_info.Map))
         # As a default we put that the direction to UP.
         # new_possibles = wallcheck(Brain.directions_possibles)
         Brain.step_compter+=1
-        return Brain.directions_possibles[Brain.step_compter%len(Brain.directions_possibles)]
+        currentPlayer = players[str(turn_info.SelfId)]
+        #return Brain.directions_possibles[Brain.step_compter%len(Brain.directions_possibles)]
+        a = Brain.wallcheck(currentPlayer.Head, Brain.directions_possibles, turn_info)
+        b = Brain.neckcheck(currentPlayer.Head, a, turn_info)
+        (b[0],b[1]) = (b[1],b[0])
+        return b[0]
         
     def on_finalized(turn_info: TurnInformation):
         '''
