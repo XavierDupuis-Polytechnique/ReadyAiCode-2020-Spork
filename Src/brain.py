d7a586917c813c72ff2c3815941259d638d2c321
Samuel Laporte <laporte.samuel2000@gmail.com>
2020-10-13 23:45:59 -0400

    Esti que je suis booste

--- Src/brain.py
+++ Src/brain.py
@@ -14,39 +14,40 @@ class Brain(metaclass=Singleton):
     counter = 0
     DIRECTIONS_POSSIBLES = [Direction._UP, Direction._DOWN, Direction._RIGHT, Direction._LEFT, Direction._NONE]
     directions_possibles = DIRECTIONS_POSSIBLES
+    retournerBody = False
 
     def ObstacleCheck(player_position_head : Vector2, possible_actions, turn_info):
         new_possible_actions = []
         map2d = np.array(turn_info.Map).reshape((turn_info.MapWidth, turn_info.MapWidth))
-        print(map2d)
+        #print(map2d)
         map2d = map2d.tolist()
-        print(player_position_head.X,player_position_head.Y)
+        #print(player_position_head.X,player_position_head.Y)
         
         j = turn_info.MapWidth - player_position_head.Y - 1
         i = player_position_head.X
-        print(i, j)
+        #print(i, j)
         for action in possible_actions:
             # Check if wall up.
             if action == Direction._UP:
-                if (map2d[j - 1][i] != 'W') and (map2d[j - 1][i] != ('p' + str(turn_info.SelfId))) and (map2d[j - 1][i] != 'B'):
+                if (map2d[j - 1][i] != 'W') and (map2d[j - 1][i] != ('p' + str(turn_info.SelfId))) and (map2d[j - 1][i] != 'B') and (map2d[j - 1][i] != 'I') and (map2d[j - 1][i] != 'D'):
                     new_possible_actions.append(action)
                     print('UP')
             # Check if wall down.
             elif action == Direction._DOWN:
-                if (map2d[j + 1][i] != 'W') and (map2d[j + 1][i] != ('p' + str(turn_info.SelfId))) and (map2d[j + 1][i] != 'B'):
+                if (map2d[j + 1][i] != 'W') and (map2d[j + 1][i] != ('p' + str(turn_info.SelfId))) and (map2d[j + 1][i] != 'B') and (map2d[j + 1][i] != 'I') and (map2d[j + 1][i] != 'D'):
                     new_possible_actions.append(action)
                     print('DOWN')
             # Check if wall left.
             elif action == Direction._LEFT:
-                if (map2d[j][i- 1] != 'W') and (map2d[j][i - 1] != ('p' + str(turn_info.SelfId))) and (map2d[j + 1][i] != 'B'):
+                if (map2d[j][i- 1] != 'W') and (map2d[j][i - 1] != ('p' + str(turn_info.SelfId))) and (map2d[j][i - 1] != 'B') and (map2d[j][i - 1] != 'I') and (map2d[j][i - 1] != 'D'):
                     new_possible_actions.append(action)
                     print('LEFT')
             # Check if wall right.
             elif action == Direction._RIGHT:
-                if (map2d[j][i + 1] != 'W') and (map2d[j][i + 1] != ('p' + str(turn_info.SelfId))) and (map2d[j + 1][i] != 'B'):
+                if (map2d[j][i + 1] != 'W') and (map2d[j][i + 1] != ('p' + str(turn_info.SelfId))) and (map2d[j][i + 1] != 'B') and (map2d[j][i + 1] != 'I') and (map2d[j][i + 1] != 'D'):
                     new_possible_actions.append(action)
                     print('RIGHT')
-        print(len(new_possible_actions))  
+        #print(len(new_possible_actions))  
         return new_possible_actions
     
     def get_players_position(turn_info):
@@ -76,39 +77,7 @@ class Brain(metaclass=Singleton):
                 r_counter -= 1
         return players_position
 
-
-    def CalculMovement(movement):
-        for i in range(50):
-            for i in range(2):
-                movement.append(Direction._UP)
-            movement.append(Direction._RIGHT)
-            movement.append(Direction._DOWN)
-            movement.append(Direction._LEFT)
-        return movement
-
-    def RetournerDepart(position_actuel, position_depart):
-        x_relatif = position_depart.X - position_actuel.X
-        y_relatif = position_depart.Y - position_actuel.Y
-        if (x_relatif < 0):
-            
-        Brain.counter = 0
-        for i in (len(movement)/5):
-            movement.append(Direction._DOWN)
-        return movement
-
-    def MovementDeBase(currentPlayer, turn_info):
-
-        movement = [Direction._UP, Direction._UP, Direction._RIGHT, Direction._DOWN, Direction._LEFT]
-        movement = Brain.CalculMovement(movement)
-        mouvementsPossibles = Brain.ObstacleCheck(currentPlayer.Head, Brain.directions_possibles, turn_info)
-        
-        if (movement[Brain.counter] not in mouvementsPossibles):
-            return mouvementsPossibles[0]
-        else:
-            Brain.counter += 1
-            return movement[Brain.counter]
-
-    def returnToBody(turn_information):
+    def IndiceBody(turn_information):
         player = Brain.get_players_position(turn_information)[str(turn_information.SelfId)]
         DistanceX =200
         DistanceY =200
@@ -117,18 +86,108 @@ class Brain(metaclass=Singleton):
         DistanceTestY = 0
         DistanceTotalTest = DistanceTestX + DistanceTestY
         indice = 0
-        for i in len(player.Body):
+        for i in range(len(player.Body)):
             DistanceTestX = player.Head.X - player.Body[i].X
             DistanceTestY = player.Head.Y - player.Body[i].Y
             DistanceTotalTest = DistanceTestX + DistanceTestY
             if(DistanceTotalTest < DistanceTotal):
                 DistanceTotal = DistanceTotalTest
                 indice = i
+        return indice
+
+    def returnToBody(mouvementsPossibles : list, turn_info):
+
+        DistanceX = Brain.get_players_position(turn_info)[str(turn_info.SelfId)].Body[Brain.IndiceBody(turn_info)].X - Brain.get_players_position(turn_info)[str(turn_info.SelfId)].Head.X
+        DistanceY = Brain.get_players_position(turn_info)[str(turn_info.SelfId)].Body[Brain.IndiceBody(turn_info)].Y - Brain.get_players_position(turn_info)[str(turn_info.SelfId)].Head.Y
         
+        if(DistanceX > 0):
+            if Direction._RIGHT in mouvementsPossibles:
+                return Direction._RIGHT
+            elif (DistanceY > 0):
+                if Direction._UP in mouvementsPossibles:
+                    return Direction._UP
+                elif Direction._DOWN in mouvementsPossibles:
+                    return Direction._DOWN
+                else:
+                    return Direction._LEFT
+            elif (DistanceY < 0):
+                if Direction._DOWN in mouvementsPossibles:
+                    return Direction._DOWN
+                elif Direction._UP in mouvementsPossibles:
+                    return Direction._UP
+                else:
+                    return Direction._LEFT
+            else:
+                if Direction._DOWN in mouvementsPossibles:
+                    return Direction._DOWN
+                elif Direction._UP in mouvementsPossibles:
+                    return Direction._UP
+                else:
+                    return Direction._RIGHT
+            
+        elif(DistanceX < 0):
+            if Direction._LEFT in mouvementsPossibles:
+                return Direction._LEFT
+            elif (DistanceY > 0):
+                if Direction._UP in mouvementsPossibles:
+                    return Direction._UP
+                elif Direction._DOWN in mouvementsPossibles:
+                    return Direction._DOWN
+                else:
+                    return Direction._RIGHT
+            elif (DistanceY < 0):
+                if Direction._DOWN in mouvementsPossibles:
+                    return Direction._DOWN
+                elif Direction._UP in mouvementsPossibles:
+                    return Direction._UP
+                else:
+                    return Direction._RIGHT
+            else:
+                if Direction._DOWN in mouvementsPossibles:
+                    return Direction._DOWN
+                elif Direction._UP in mouvementsPossibles:
+                    return Direction._UP
+                else:
+                    return Direction._LEFT
+            
 
+        elif (DistanceX == 0):
+            if (DistanceY < 0):
+                if Direction._DOWN in mouvementsPossibles:
+                    return Direction._DOWN
+                elif Direction._UP in mouvementsPossibles:
+                    return Direction._UP
+                elif Direction._LEFT in mouvementsPossibles:
+                    return Direction._LEFT
+                else:
+                    return Direction._RIGHT
+            elif (DistanceY > 0):
+                if Direction._UP in mouvementsPossibles:
+                    return Direction._UP
+                elif Direction._DOWN in mouvementsPossibles:
+                    return Direction._DOWN
+                elif Direction._LEFT in mouvementsPossibles:
+                    return Direction._LEFT
+                else:
+                    return Direction._RIGHT        
 
+    def DistanceBody(turn_information):
+        player = Brain.get_players_position(turn_information)[str(turn_information.SelfId)]
+        # DistanceX = 200
+        # DistanceY = 200
+        # DistanceTotal = DistanceX + DistanceY
+        # DistanceTestX = 0
+        # DistanceTestY = 0
+        # DistanceTotalTest = DistanceTestX + DistanceTestY
+        # for i in range(len(player.Body)):
+        #     DistanceTestX = player.Head.X - player.Body[i].X
+        #     DistanceTestY = player.Head.Y - player.Body[i].Y
+        #     DistanceTotalTest = DistanceTestX + DistanceTestY
+        #     if(DistanceTotalTest < DistanceTotal):
+        #         DistanceTotal = DistanceTotalTest
+        return len(player.Neck)
 
-
+    
 
     
     def on_next_move(turn_info: TurnInformation):
@@ -137,14 +196,18 @@ class Brain(metaclass=Singleton):
         @param turn_info: Information from the current turn
         @return: The direction your AI chose as his next move.
         '''
-        # Brain.directions_possibles = Brain.DIRECTIONS_POSSIBLES
-        # players = Brain.get_players_position(turn_info) 
-        # currentPlayer = players[str(turn_info.SelfId)]
-
-        # return Brain.MovementDeBase(currentPlayer, turn_info)
-        #return Brain.ObstacleCheck(currentPlayer.Head, Brain.directions_possibles, turn_info)[0]
-
-        return random.choice(Brain.ObstacleCheck(Brain.directions_possibles))
+        currentPlayer = Brain.get_players_position(turn_info)[str(turn_info.SelfId)]
+        print(Brain.DistanceBody(turn_info))
+        if(Brain.DistanceBody(turn_info) >= 4):
+            Brain.retournerBody = True
+        if(Brain.DistanceBody(turn_info) == 0):
+            Brain.retournerBody = False
+        print("the game has ended and the received id from the game server is {0} with the following map {1}".format(turn_info.SelfId, turn_info.Map)) 
+        while Brain.retournerBody == True:
+            print("Je dois retourner au BODY")
+            return Brain.returnToBody(Brain.ObstacleCheck(currentPlayer.Head, Brain.DIRECTIONS_POSSIBLES, turn_info), turn_info)
+        
+        return random.choice(Brain.ObstacleCheck(currentPlayer.Head, Brain.DIRECTIONS_POSSIBLES, turn_info))        
 
     def on_finalized(turn_info: TurnInformation):
         '''
